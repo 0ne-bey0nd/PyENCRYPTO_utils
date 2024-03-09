e47baf0875283993a5db36fc1f5f09afda34004d
0neArt <one@art.net>
2024-03-09 12:16:08 +0800

    fix: typedefs_test.py 完善

--- test/PyENCRYPTO_utils/typedefs_test.py
+++ test/PyENCRYPTO_utils/typedefs_test.py
@@ -1,7 +1,11 @@
 from PyENCRYPTO_utils.typedefs import *
 import unittest
 
+
 class TypedefsTest(unittest.TestCase):
 
     def test_test(self):
-        self.assertEqual(REGISTER_SIZE, 64)
+        self.assertEqual(REGISTER_SIZE, Uint64)
+
+        self.assertEqual(REGISTER_SIZE.size(), 64)
+        # self.assertEqual(LOG2_REGISTER_SIZE, ceil_log2(REGISTER_SIZE.size()))
